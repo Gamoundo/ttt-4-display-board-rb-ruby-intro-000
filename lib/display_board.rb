9c5abee9fe89e098abec235b314bb31480c44d3f
Akil Griffiths <akilgg@gmail.com>
2020-04-05 16:10:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,29 +1,27 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(array)
+  cell = ["   ", "   ", "   "]
+ cell2 = ["   ", "   ", "   "]
+ cell3 = ["   ", "   ", "   "]
+
+cell[0] = " #{array[0]} "
+cell[1] = " #{array[1]} "
+cell[2] = " #{array[2]} "
+cell2[0] = " #{array[3]} "
+cell2[1] = " #{array[4]} "
+cell2[2] = " #{array[5]} "
+cell3[0] = " #{array[6]} "
+cell3[1] = " #{array[7]} "
+cell3[2] = " #{array[8]} "
+
+puts [cell.*("|"), "-----------" ]
+  puts [cell2.*("|"), "-----------" ]
+  puts cell3.*("|")
 
 
-def display_board(board, array)
-  def board
-   cell = ["   ", "   ", "   "]
-  cell2 = cell.*("|")
-  cell3 = cell2
-  cell4 = cell3
-    puts [cell2, "-----------" ]
-    puts [cell3, "-----------" ]
-    puts cell4
-
-
-
-  end
-  cell2[0] = "#{array[0]}"
-  cell2[1] = "#{array[1]}"
-  cell2[2] = "#{array[2]}"
-  cell3[0] = "#{array[3]}"
-  cell3[1] = "#{array[4]}"
-  cell3[2] = "#{array[5]}"
-  cell4[0] = "#{array[6]}"
-  cell4[1] = "#{array[7]}"
-  cell4[2] = "#{array[8]}"
-puts board
-  print board
 end
+
+
+
+
