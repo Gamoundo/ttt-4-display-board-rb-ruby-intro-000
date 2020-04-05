4fd0a0cc569664907fb64dfa99a51ee03e47268d
Akil Griffiths <akilgg@gmail.com>
2020-04-05 14:46:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,28 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def board
+ cell = ["   ", "   ", "   "]
+cell2 = cell.*("|")
+cell3 = cell2
+cell4 = cell3
+  puts [cell2, "-----------" ]
+  puts [cell3, "-----------" ]
+  puts cell4
+
+
+
+end
+
+def display_board(board, array)do
+  cell2[0] = array[0]
+  cell2[1] = array[1]
+  cell2[2] = array[2]
+  cell3[0] = array[3]
+  cell3[1] = array[4]
+  cell3[2] = array[5]
+  cell4[0] = array[6]
+  cell4[1] = array[7]
+  cell4[2] = array[8]
+  print board
+end
+  
