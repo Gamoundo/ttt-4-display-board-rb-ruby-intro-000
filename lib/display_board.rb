e797f30cc39ee1b823317f21ce2d3e884c61ad7d
Akil Griffiths <akilgg@gmail.com>
2020-04-05 15:03:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,15 +15,15 @@ def display_board(board, array)
 
 
   end
-  cell2[0] = array[0]
-  cell2[1] = array[1]
-  cell2[2] = array[2]
-  cell3[0] = array[3]
-  cell3[1] = array[4]
-  cell3[2] = array[5]
-  cell4[0] = array[6]
-  cell4[1] = array[7]
-  cell4[2] = array[8]
+  cell2[0] = "#{array[0]}"
+  cell2[1] = "#{array[1]}"
+  cell2[2] = "#{array[2]}"
+  cell3[0] = "#{array[3]}"
+  cell3[1] = "#{array[4]}"
+  cell3[2] = "#{array[5]}"
+  cell4[0] = "#{array[6]}"
+  cell4[1] = "#{array[7]}"
+  cell4[2] = "#{array[8]}"
 puts board
   print board
 end
